1b5358c177c7e1fac369d04cba4228abade1f270
Yoonhwi <ush0105@naver.com>
2025-02-05 13:09:22 +0900

    feat: Home페이지 Popular Plans 및 formatPrice함수 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { BaseLayout } from "@/layouts";
+import { formatPrice } from "@/utils";
+import { TiHeartFullOutline } from "react-icons/ti";
 import styles from "./Home.module.css";
 import { descriptionItems } from "./constants";
 
@@ -8,25 +10,43 @@ const HomePage = () => {
       <div className={styles.container}>
         <img src="/banner_img.jpg" alt="banner" className={styles.banner_img} />
 
-        <div className={styles.des_container}>
-          <p className={styles.title}>WHERE TO GO?</p>
-          <div className={styles.divider} />
-          <div className={styles.des_img_container}>
-            {descriptionItems.map((item) => {
-              return (
-                <div className={styles.img_relative_box} key={item.title}>
-                  <p className={styles.des_img_title}>{item.title}</p>
-                  <img
-                    src={item.imgSrc}
-                    alt="destination"
-                    className={styles.des_img}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles.divider} />
-        </div>
+        {descriptionItems.map((item) => {
+          const { title, data } = item;
+
+          return (
+            <div key={title} className={styles.des_container}>
+              <p className={styles.title}>{title}</p>
+              <div className={styles.divider} />
+
+              <div className={styles.des_img_container}>
+                {data.map((item) => {
+                  const isDetail = item.price && item.like_count;
+                  return (
+                    <div className={styles.img_relative_box} key={item.name}>
+                      <p className={styles.des_img_title}>{item.name}</p>
+                      <img
+                        src={item.imgSrc}
+                        alt="destination"
+                        className={styles.des_img}
+                      />
+                      {isDetail && (
+                        <div className={styles.des_detail}>
+                          <p>{formatPrice(item.price)}₩</p>
+                          <div className={styles.like_box}>
+                            <TiHeartFullOutline className={styles.icon_heart} />
+                            <p>{item.like_count}</p>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className={styles.divider} />
+            </div>
+          );
+        })}
       </div>
     </BaseLayout>
   );
