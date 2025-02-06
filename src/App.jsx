6a6d0f26ffc7df504e29f35f58af93b85d71f79a
Yoonhwi <ush0105@naver.com>
2025-02-06 15:24:53 +0900

    feat: Toast 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Test, AuthLayout } from "@/pages";
 import { PageEndPoints } from "@/constants";
-import { DarkModeProvider } from "@/contexts";
+import { DarkModeProvider, ToastProvider } from "@/contexts";
+import { ToastList } from "./components";
 
 function App() {
   return (
     <DarkModeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path={PageEndPoints.HOME} element={<Home />} />
-          <Route path={PageEndPoints.TEST} element={<Test />} />
-          <Route path={PageEndPoints.AUTH} element={<AuthLayout />} />
-        </Routes>
-      </BrowserRouter>
+      <ToastProvider>
+        <ToastList />
+        <BrowserRouter>
+          <Routes>
+            <Route path={PageEndPoints.HOME} element={<Home />} />
+            <Route path={PageEndPoints.TEST} element={<Test />} />
+            <Route path={PageEndPoints.AUTH} element={<AuthLayout />} />
+          </Routes>
+        </BrowserRouter>
+      </ToastProvider>
     </DarkModeProvider>
   );
 }
