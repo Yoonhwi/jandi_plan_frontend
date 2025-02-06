e740d51b549c203c464f3b6f2e740e3f78256de1
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-02-06 20:42:28 +0900

    chore: input type 설정정

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import styles from "./Input.module.css";
 
-const Input =({ size = "lg", placeholder= "입력하세요", children, ...props }) => {
+const Input =({ size = "lg", placeholder= "입력하세요", type="text", children, ...props }) => {
 
     const sizeClass = styles[`input_${size}`];
 
     return(
-        <input className={`${sizeClass} ${styles.input}`} placeholder={placeholder}{...props} />
+        <input className={`${sizeClass} ${styles.input}`} placeholder={placeholder} type={type} {...props} />
     );
 };
 
