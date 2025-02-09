8a5a47f8b7d804198a564fec236bb3811f3b4d78
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-02-09 22:06:08 +0900

    feat: 날짜 선택시 modal창 닫기 기능 추가

--- src/pages/Plan/CreatePlan/Constants/Calender.jsx
+++ src/pages/Plan/CreatePlan/Constants/Calender.jsx
@@ -1,13 +1,16 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
+import { useModal } from "@/components/Modal/ModalContext";
 
 const Calender = ({ onSelectDate }) => {
   const [selectedDate, setSelectedDate] = useState(null);
+  const { closeModal } = useModal();
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
     onSelectDate(date.toLocaleDateString()); // 선택한 날짜 전달
+    closeModal();
   };
 
   return (
