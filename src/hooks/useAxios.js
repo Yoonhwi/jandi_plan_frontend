f6384c6c98b90c7938fb449d77371058c51c0817
Yoonhwi <ush0105@naver.com>
2025-02-21 01:23:18 +0900

    chore: useAxios 반환값에 isSuccess추가 및 상태 관리 최적화

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -7,27 +7,34 @@ import { useCallback, useState } from "react";
  *   loading: boolean,
  *   response: any,
  *   error: any,
+ *   isSuccess: boolean,
  *   fetchData: (params?: Object) => Promise<void>;
  * }}
  */
 const useAxios = () => {
-  const [response, setResponse] = useState();
-  const [error, setError] = useState();
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const fetchData = useCallback(async (params) => {
     setLoading(true);
+    setIsSuccess(false);
+    setError(null);
+
     try {
       const res = await axiosInstance(params);
       setResponse(res.data);
+      setIsSuccess(true);
     } catch (err) {
       setError(err);
+      setResponse(null);
     } finally {
       setLoading(false);
     }
   }, []);
 
-  return { loading, response, error, fetchData };
+  return { loading, response, error, isSuccess, fetchData };
 };
 
 export default useAxios;
