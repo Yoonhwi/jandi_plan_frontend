e3af05956673d69ee0efba13b3dcf47c34398df6
Yoonhwi <ush0105@naver.com>
2025-02-25 18:05:43 +0900

    feat: 내 여행계획 api연결

---
+++ src/pages/MyPage/MyPlan/MyPlan.jsx
@@ -0,0 +1,61 @@
+import { Slider } from "@/components";
+import styles from "./MyPlan.module.css";
+import { useAxios } from "@/hooks";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { APIEndPoints } from "@/constants";
+import DetailItem from "./DetailItem";
+
+const MyPlan = () => {
+  const [data, setData] = useState([]);
+  const { fetchData } = useAxios();
+  const accessToken = localStorage.getItem("access-token");
+
+  const hash = useMemo(() => new Set(), []);
+
+  const getMyPlanList = useCallback(async () => {
+    await fetchData({
+      url: APIEndPoints.TRIP_MY,
+      method: "GET",
+      params: {
+        page: 0,
+        size: 10,
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }).then((res) => {
+      const itesm = res.data.items;
+      const filtered = itesm.filter((item) => {
+        if (hash.has(item.tripId)) return false;
+        hash.add(item.tripId);
+        return true;
+      });
+
+      setData((prev) => {
+        return [...prev, ...filtered];
+      });
+    });
+  }, [accessToken, fetchData, hash]);
+
+  useEffect(() => {
+    getMyPlanList();
+  }, [getMyPlanList]);
+
+  return (
+    <div className={styles.myplan_box}>
+      <div className={styles.title_box}>
+        <p className={styles.title}>여행 계획</p>
+      </div>
+
+      {data.length > 0 ? (
+        <Slider items={data} size="md">
+          {(item) => <DetailItem key={item.tripId} item={item} />}
+        </Slider>
+      ) : (
+        <p>여행 계획이 없습니다.</p>
+      )}
+    </div>
+  );
+};
+
+export default MyPlan;
