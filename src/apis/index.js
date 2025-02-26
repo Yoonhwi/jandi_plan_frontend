ace8b58f7e64357e9f64f47efc0b393dea5e715f
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-02-26 19:36:59 +0900

    Refactor: login refactoring 진행행

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,1 +1,2 @@
 export { default as axiosInstance } from "./axiosInstance";
+export { default as AuthService } from "./auth";
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,38 +1,34 @@
 import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { APIEndPoints, PageEndPoints } from "@/constants";
-import { useAxios } from "@/hooks";
+import { AuthService } from "@/apis";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
-  const { loading, fetchData, response } = useAxios();
+  const { login, refreshAccessToken, getUserInfo} = AuthService;
   const navigate = useNavigate();
   const location = useLocation();
 
-  const signIn = (data) => {
-        fetchData({
-        method: "POST",
-        url: `${APIEndPoints.LOGIN}`,
-        data: {
-          email: data.id,
-          password: data.password, 
-        },
-      }).then((res)=>{
-        console.log(res);
-        localStorage.setItem("access-token", res.data.accessToken);
-        localStorage.setItem("refresh-token", res.data.refreshToken);
-        setIsLoggedIn(true);
-        userInfo(res.data.accessToken);
-
-        const redirectPath = location.state?.from || PageEndPoints.HOME;
-        navigate(redirectPath, { replace: true });
-
-      }).catch((err) => {
-        console.error(err);
-        setIsLoggedIn(false);
-      });
+  const signIn = async (data) => {
+    try {
+      const tokens = await login(data); 
+    
+      localStorage.setItem("access-token", tokens.accessToken);
+      localStorage.setItem("refresh-token", tokens.refreshToken);
+  
+      setIsLoggedIn(true);
+      const userInfo = getUserInfo(tokens.accessToken);
+      setUser(userInfo);
+  
+      // 로그인 후 리디렉션 처리
+      const redirectPath = location.state?.from || PageEndPoints.HOME;
+      navigate(redirectPath, { replace: true });
+    } catch (err) {
+      setIsLoggedIn(false);
+      console.log(err.message);
+    }
   }
 
   const signOut = () => {
@@ -40,21 +36,6 @@ const AuthProvider = ({ children }) => {
     localStorage.removeItem("refresh-token");
     setIsLoggedIn(false);
     window.location.reload();
-  }
-
-  const userInfo = async (token) => {
-    fetchData({
-      method: "GET",
-      url: `${APIEndPoints.PROFILE}`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((res)=>{
-      console.log(res.data);
-      setUser(res.data);
-    }).catch((err) => {
-      console.error(err);
-    });
   }
 
   useEffect(() => {
@@ -66,31 +47,36 @@ const AuthProvider = ({ children }) => {
   
       if (!accessToken && refreshToken) {
         try {
-          const res = await fetchData({
-            method: "POST",
-            url: `${APIEndPoints.REFRESH}`,
-            data: { refreshToken },
-          });
-  
-          if (res?.data.accessToken) {
+          const token = await refreshAccessToken(refreshToken); 
+
+          console.log(token);
+        
+          if (token?.accessToken){
             console.log("로그인다시");
-            localStorage.setItem("access-token", res.data.accessToken);
-            localStorage.setItem("refresh-token", res.data.refreshToken);
+            localStorage.setItem("access-token", token.accessToken);
+            localStorage.setItem("refresh-token", token.refreshToken);
             setIsLoggedIn(true);
-            userInfo(res.data.accessToken);
-          } else {
+            const userInfo = getUserInfo(token.accessToken);
+            setUser(userInfo);
+          }else {
             throw new Error("새로운 accessToken을 받지 못함");
           }
-        } catch (error) {
-          console.error("토큰 갱신 실패:", error);
+      
+          // 로그인 후 리디렉션 처리
+          const redirectPath = location.state?.from || PageEndPoints.HOME;
+          navigate(redirectPath, { replace: true });
+        } catch (err) {
           setIsLoggedIn(false);
+          console.error("토큰 갱신 실패:", err);
           signOut();
           navigate(PageEndPoints.LOGIN, { state: { from: location.pathname } });
         }
+
       } else {
         console.log("로그인되어있음");
         setIsLoggedIn(true);
-        userInfo(accessToken);
+        const userInfo = getUserInfo(accessToken);
+        setUser(userInfo);
       }
     };
   
