a7bc24f94183c4eca75e0e3a17dbe6f5c9a69eb0
Yoonhwi <ush0105@naver.com>
2025-02-27 06:27:31 +0900

    chore: CreatePlan 폴더구조 수정 및 리팩토링

--- src/pages/Plan/CreatePlan/ModalContents/AddUser.jsx
+++ src/pages/Plan/CreatePlan/ModalContents/AddUser.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import styles from "./AddUser.module.css";
-import {Input, Button} from "@/components";
+import { Input, Button } from "@/components";
 import { useModal } from "@/components/Modal/ModalContext";
 
-const AddUser = ({onConfirm}) => {
+const AddUser = ({ onConfirm }) => {
   const [inputValue, setInputValue] = useState("");
   const [userNames, setUserNames] = useState([]);
   const { closeModal } = useModal();
 
   const handleAddUser = () => {
-    if (inputValue.trim() === "") return;  // 빈 값 방지 나중에 이름 존재 여부 확인
+    if (inputValue.trim() === "") return; // 빈 값 방지 나중에 이름 존재 여부 확인
 
     setUserNames([...userNames, inputValue]);
     setInputValue("");
@@ -20,7 +20,7 @@ const AddUser = ({onConfirm}) => {
   };
 
   const handleConfirmClick = () => {
-    onConfirm(userNames); 
+    onConfirm(userNames);
     closeModal();
   };
 
@@ -29,20 +29,35 @@ const AddUser = ({onConfirm}) => {
       <div className={styles.title}>친구 추가하기</div>
       <div className={styles.input_box}>
         <div className={styles.input}>
-          <Input size="lg" placeholder="친구 이름" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+          <Input
+            size="lg"
+            placeholder="친구 이름"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </div>
-        <Button size="md" onClick={handleAddUser}>친구 추가</Button>
+        <Button size="md" onClick={handleAddUser}>
+          친구 추가
+        </Button>
       </div>
       <div className={styles.user_name_box}>
         {userNames.map((name, index) => (
           <div key={index} className={styles.user_name}>
             <p className={styles.name}>{name}</p>
-            <Button size="sm" variant="none" onClick={() => handleRemoveUser(index)}>X</Button>
+            <Button
+              size="sm"
+              variant="none"
+              onClick={() => handleRemoveUser(index)}
+            >
+              X
+            </Button>
           </div>
         ))}
       </div>
       <div className={styles.add_button_box}>
-        <Button size="md" onClick={handleConfirmClick}>친구 추가</Button>
+        <Button size="md" onClick={handleConfirmClick}>
+          친구 추가
+        </Button>
       </div>
     </div>
   );
