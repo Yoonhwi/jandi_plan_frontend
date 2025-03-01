1cee33ba327b10a600bebc3f084c168d2d1359cf
Yoonhwi <ush0105@naver.com>
2025-03-01 23:29:07 +0900

    feat: 일자별 일정을 위해 provider내에 flattendItinerary추가

--- src/pages/Plan/Detail/PlanDetailContext.jsx
+++ src/pages/Plan/Detail/PlanDetailContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useContext } from "react";
 
 export const PlanDetailContext = createContext({
-  tripDetail: null,
-  focusDay: null,
-  tripItinerary: null,
-  tripReservation: null,
-  setFocusDay: () => {},
-  addReservation: () => {},
-  addSchedule: () => {},
+  tripDetail: null, // 여행 계획의 기본 정보
+  focusDay: null, // 현재 보고 있는 일자
+  tripItinerary: null, // 여행 계획의 일정 정보
+  tripReservation: null, // 여행 계획의 예약 정보
+  setFocusDay: () => {}, // 현재 보고 있는 일자를 변경하는 함수
+  addReservation: () => {}, // 여행 계획에 예약 정보를 추가하는 함수
+  addItinerary: () => {}, // 여행 계획에 일정 정보를 추가하는 함수
+  flattendItinerary: [], // 여행 계획의 일정 정보를 날짜별로 정리한 정보
 });
 
 export const usePlanDetail = () => {
--- src/pages/Plan/Detail/PlanDetailProvider.jsx
+++ src/pages/Plan/Detail/PlanDetailProvider.jsx
@@ -6,6 +6,7 @@ import { buildPath } from "@/utils";
 import { APIEndPoints } from "@/constants";
 
 const PlanDetailProvider = ({ children }) => {
+  const [flattendItinerary, setFlattendItinerary] = useState([]);
   const [focusDay, setFocusDay] = useState(null);
   const { id } = useParams();
 
@@ -13,44 +14,49 @@ const PlanDetailProvider = ({ children }) => {
   const { response: tripItinerary, fetchData: tripItineraryFetch } = useAxios();
   const { response: tripReservation, fetchData: tripReservationFetch } =
     useAxios();
+
   const { fetchData: postReservation } = useAxios();
+  const { fetchData: postItinerary } = useAxios();
 
-  console.log("tripItinerary", tripItinerary);
-  console.log("tripReservation", tripReservation);
-
+  // 여행 계획의 기본 정보를 가져오는 함수
   const fetchPlanDetail = useCallback(async () => {
     const url = buildPath(APIEndPoints.TRIP_DETAIL, { id });
     await tripFetch({ url, method: "GET" });
   }, [id, tripFetch]);
 
+  // 여행 계획의 일정을 가져오는 함수
   const fetchPlanItinerary = useCallback(async () => {
     const url = buildPath(APIEndPoints.TRIP_ITINERARY, { id });
     await tripItineraryFetch({ url, method: "GET" });
   }, [id, tripItineraryFetch]);
 
+  // 여행 계획의 예약 정보를 가져오는 함수
   const fetchPlanReservation = useCallback(async () => {
     const url = buildPath(APIEndPoints.TRIP_RESERVATION, { id });
     await tripReservationFetch({ url, method: "GET" });
   }, [id, tripReservationFetch]);
 
+  // 여행 계획에 예약 정보를 추가하는 함수
   const addReservation = useCallback(
     (data) => {
       const url = buildPath(APIEndPoints.TRIP_RESERVATION, { id });
-      postReservation({ url, method: "POST", params: data }).then(() => {
-        fetchPlanReservation();
-      });
+      postReservation({ url, method: "POST", data }).then(() =>
+        fetchPlanReservation()
+      );
     },
     [fetchPlanReservation, id, postReservation]
   );
 
-  const addSchedule = useCallback(
+  // 여행 계획에 일정 정보를 추가하는 함수
+  const addItinerary = useCallback(
     (data) => {
+      console.log("data", data);
       const url = buildPath(APIEndPoints.TRIP_ITINERARY, { id });
-      tripItineraryFetch({ url, method: "POST", data }).then(() => {
-        fetchPlanItinerary();
-      });
+      postItinerary({ url, method: "POST", data }).then(() =>
+        fetchPlanItinerary()
+      );
     },
-    [fetchPlanItinerary, id, tripItineraryFetch]
+    [fetchPlanItinerary, id, postItinerary]
   );
 
   useEffect(() => {
@@ -58,6 +64,32 @@ const PlanDetailProvider = ({ children }) => {
     fetchPlanItinerary();
     fetchPlanReservation();
   }, [fetchPlanDetail, fetchPlanItinerary, fetchPlanReservation]);
+
+  // 일정 정보를 날짜별로 정리하는 코드
+  useEffect(() => {
+    if (!tripItinerary || !tripDetail) return;
+
+    const { startDate, endDate } = tripDetail;
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const temp = [];
+    while (start <= end) {
+      const date = start.toISOString().split("T")[0];
+      const day =
+        Math.floor((start - new Date(startDate)) / (1000 * 60 * 60 * 24)) + 1;
+      temp.push({ date, day, data: [], cost: 0 });
+      start.setDate(start.getDate() + 1);
+    }
+
+    tripItinerary.forEach((item) => {
+      const { date, cost } = item;
+      const index = temp.findIndex((day) => day.date === date);
+      temp[index].data.push(item);
+      temp[index].cost += cost;
+    });
+
+    setFlattendItinerary(temp);
+  }, [tripItinerary, tripDetail]);
 
   return (
     <PlanDetailContext.Provider
@@ -68,7 +100,8 @@ const PlanDetailProvider = ({ children }) => {
         tripItinerary,
         tripReservation,
         addReservation,
-        addSchedule,
+        addItinerary,
+        flattendItinerary,
       }}
     >
       {children}
