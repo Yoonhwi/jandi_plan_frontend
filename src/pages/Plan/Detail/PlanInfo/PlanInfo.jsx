9618d26fc588341b3f3fd6ba89b7d1001d217d40
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-03-07 00:35:59 +0900

    feat: plan페이지 좋아요 db 연결

--- src/pages/Plan/Detail/PlanInfo/PlanInfo.jsx
+++ src/pages/Plan/Detail/PlanInfo/PlanInfo.jsx
@@ -3,7 +3,7 @@ import { APIEndPoints } from "@/constants";
 import { useToast } from "@/contexts";
 import { useAxios } from "@/hooks";
 import { buildPath } from "@/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsPersonArmsUp } from "react-icons/bs";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
@@ -15,9 +15,13 @@ import styles from "./PlanInfo.module.css";
 
 const PlanInfo = (user) => {
   const { tripDetail } = usePlanDetail();
-  const [liked, setLiked] = useState(false); //나중에 trip좋아요 정보 넘어오면 여기 수정
+  const [liked, setLiked] = useState(false); 
   const { createToast } = useToast();
   const { fetchData: postApi } = useAxios();
+
+  useEffect(()=>{
+    setLiked(tripDetail?.liked);
+  },[tripDetail])
 
   const likedTrip = (setMethod, id) => {
     postApi({
@@ -25,6 +29,10 @@ const PlanInfo = (user) => {
       url: buildPath(APIEndPoints.TRIP_SET_LIKED, { id }),
     })
       .then(() => {
+        createToast({
+          type: "success",
+          text: "좋아요가 변경되었습니다",
+        });
         setLiked((prev) => !prev);
       })
       .catch((err) => {
